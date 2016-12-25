884550b23bf944c9f25231a0699abf3739a8fbee
Monica Gallardo <mcgv2004@hotmail.com>
2016-12-24 22:51:23 -0300

    commit

--- vecinos/settings.py
+++ vecinos/settings.py
@@ -127,9 +127,14 @@ USE_TZ = True
 
 
 # Parse database configuration from $DATABASE_URL
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
+
+
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
- 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
  
@@ -144,4 +149,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+
 
